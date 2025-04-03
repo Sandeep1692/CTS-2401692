dc3782d6f7fedef4a3d24400f539b7b8975b3fe8
Sandeep1692 <Mopidevi.DurgaSaiSandeep@cognizant.com>
2025-04-03 10:55:44 +0530

    Refactor List-b program to accept user input for names and numbers, and store them in a file

--- CTS-Training/April-02-2025/List-b/List-b/Program.cs
+++ CTS-Training/April-02-2025/List-b/List-b/Program.cs
@@ -16,69 +16,140 @@
 //} 
 
 
-Console.WriteLine("Hello World!");
-List<string> names = new List<string>();
-List<int> numbers = new List<int>();
+//Console.WriteLine("Hello World!");
+//List<string> names = new List<string>();
+//List<int> numbers = new List<int>();
 
 
-names.Add("Sandeep");
-names.Add("Vishal");
-names.Add("Sachin");
+//names.Add("Sandeep");
+//names.Add("Vishal");
+//names.Add("Sachin");
 
-numbers.Add(1);
-numbers.Add(2);
-numbers.Add(3);
-numbers.Add(4);
-numbers.Add(5);
-int countofnames = 0;
-int CalCount(List<string> names)
+//numbers.Add(1);
+//numbers.Add(2);
+//numbers.Add(3);
+//numbers.Add(4);
+//numbers.Add(5);
+//int countofnames = 0;
+//int CalCount(List<string> names)
+//{
+//    for (int i = 0; i < 100; i++)
+//    {
+//        try
+//        {
+//            if (names[i] != null)
+//            {
+//                countofnames++;
+//            }
+//        }
+//        catch (Exception ex)
+//        {
+//        }
+//    }
+//    return countofnames;
+//}
+//int CalNum(List<int> numbers)
+//{
+//    int countofnumbers = 0;
+//    for (int i = 0; i < 100; i++)
+//    {
+//        try
+//        {
+//            if (numbers[i] != 0)
+//            {
+//                countofnumbers++;
+//            }
+//        }
+//        catch (Exception ex)
+//        {
+//        }
+//    }
+//    return countofnumbers;
+//}
+
+//int countnames = CalCount(names);
+//Console.WriteLine("Count Names:" +countnames);
+//int countnumbers = CalNum(numbers);
+//Console.WriteLine("Count Numbers:"+ countnumbers);
+
+//Console.WriteLine();
+//for (int i = 0; i < countnames; i++)
+//{
+//    Console.WriteLine(names[i]);
+//}
+
+//for (int i = 0; i < countnumbers; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class MainClass
 {
-    for (int i = 0; i < 100; i++)
+    static void Main()
     {
-        try
+        Console.WriteLine("Hello Jay!");
+
+        List<string> names = new List<string>();
+        List<int> numbers = new List<int>();
+
+        Console.Write("Enter the number of names you want to input: ");
+        int numNames = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numNames; i++)
         {
-            if (names[i] != null)
+            Console.Write("Enter a name: ");
+            names.Add(Console.ReadLine());
+        }
+
+        Console.Write("Enter the number of numbers you want to input: ");
+        int numNumbers = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numNumbers; i++)
+        {
+            Console.Write("Enter a number: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        int countnames = names.Count;
+        int countnumbers = numbers.Count;
+
+        Console.WriteLine("Count Names: " + countnames);
+        Console.WriteLine("Count Numbers: " + countnumbers);
+
+        Console.WriteLine("\nNames:");
+        for (int i = 0; i < countnames; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+
+        Console.WriteLine("\nNumbers:");
+        for (int i = 0; i < countnumbers; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+
+        StoreInputToFile(names, numbers);
+        Console.WriteLine("Input has been stored in user_input.txt");
+    }
+
+    static void StoreInputToFile(List<string> names, List<int> numbers, string filename = "user_input.txt")
+    {
+        using (StreamWriter file = new StreamWriter(filename))
+        {
+            file.WriteLine("Names:");
+            foreach (var name in names)
             {
-                countofnames++;
+                file.WriteLine(name);
+            }
+
+            file.WriteLine("\nNumbers:");
+            foreach (var number in numbers)
+            {
+                file.WriteLine(number);
             }
         }
-        catch (Exception ex)
-        {
-        }
     }
-    return countofnames;
-}
-int CalNum(List<int> numbers)
-{
-    int countofnumbers = 0;
-    for (int i = 0; i < 100; i++)
-    {
-        try
-        {
-            if (numbers[i] != 0)
-            {
-                countofnumbers++;
-            }
-        }
-        catch (Exception ex)
-        {
-        }
-    }
-    return countofnumbers;
-}
-
-int countnames = CalCount(names);
-Console.WriteLine("Count Names:" +countnames);
-int countnumbers = CalNum(numbers);
-Console.WriteLine("Count Numbers:"+ countnumbers);
-
-Console.WriteLine();
-for (int i = 0; i < countnames; i++)
-{
-    Console.WriteLine(names[i]);
-}
-
-for (int i = 0; i < countnumbers; i++)
-{
-    Console.WriteLine(numbers[i]);
 }
---
+++ CTS-Training/April-03-2025/ClassesPractice/ClassesPractice/Program.cs
@@ -0,0 +1,126 @@
+﻿//Crocodile croc = new Crocodile();
+
+//croc.weight = 450;
+//croc.speciesname = "Mugger Crocodile";
+//croc.scientificname = "Crocodylus palustris";
+//croc.length = 11;
+//croc.isExtinct = false;
+//croc.ExtinctStatus = "Vulnerable";
+//croc.Lifespan = 70;
+//croc.DisplayDetails();
+
+//class Crocodile
+//{
+//    public int weight { get; set; }
+
+//    public string speciesname { get; set; }
+//    public string scientificname { get; set; }
+//    public double length { get; set; }
+//    public bool isExtinct { get; set; }
+//    public string ExtinctStatus { get; set; }
+//    public int Lifespan { get; set; }
+
+//    public void DisplayDetails()
+//    {
+//        Console.WriteLine($"Species Name: {speciesname}");
+//        Console.WriteLine($"Scientific Name: {scientificname}");
+//        Console.WriteLine($"Weight: {weight} kg");
+//        Console.WriteLine($"Length: {length} feet");
+//        Console.WriteLine($"Is Extinct: {isExtinct}");
+//        Console.WriteLine($"Extinct Status: {ExtinctStatus}");
+//        Console.WriteLine($"Lifespan: {Lifespan} years");
+//    }
+//}
+
+Console.Write("Enter the number of crocodiles to collect: ");
+int n = int.Parse(Console.ReadLine());
+
+CrocodileCollection crocodileCollection = new CrocodileCollection();
+crocodileCollection.CollectNCrocodiles(n);
+crocodileCollection.DisplayNCrocodiles();
+class Crocodile
+{
+    public int weight { get; set; }
+    public string speciesname { get; set; }
+    public string scientificname { get; set; }
+    public double length { get; set; }
+    public bool isExtinct { get; set; }
+    public string ExtinctStatus { get; set; }
+    public int Lifespan { get; set; }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Species Name: {speciesname}");
+        Console.WriteLine($"Scientific Name: {scientificname}");
+        Console.WriteLine($"Weight: {weight} kg");
+        Console.WriteLine($"Length: {length} feet");
+        Console.WriteLine($"Is Extinct: {isExtinct}");
+        Console.WriteLine($"Extinct Status: {ExtinctStatus}");
+        Console.WriteLine($"Lifespan: {Lifespan} years");
+    }
+
+    public static Crocodile CollectCrocodileDetails()
+    {
+        Crocodile croc = new Crocodile();
+        Console.WriteLine("Enter details for the crocodile:");
+        Console.Write("Weight (kg): ");
+        croc.weight = int.Parse(Console.ReadLine());
+        Console.Write("Species Name: ");
+        croc.speciesname = Console.ReadLine();
+        Console.Write("Scientific Name: ");
+        croc.scientificname = Console.ReadLine();
+        Console.Write("Length (feet): ");
+        croc.length = double.Parse(Console.ReadLine());
+        Console.Write("Is Extinct (true/false): ");
+        croc.isExtinct = bool.Parse(Console.ReadLine());
+        Console.Write("Extinct Status: ");
+        croc.ExtinctStatus = Console.ReadLine();
+        Console.Write("Lifespan (years): ");
+        croc.Lifespan = int.Parse(Console.ReadLine());
+        return croc;
+    }
+
+    public static bool CheckCrocodileDetails(Crocodile croc)
+    {
+        if (croc.weight <= 0 || string.IsNullOrEmpty(croc.speciesname) || string.IsNullOrEmpty(croc.scientificname) ||
+            croc.length <= 0 || string.IsNullOrEmpty(croc.ExtinctStatus) || croc.Lifespan <= 0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
+class CrocodileCollection
+{
+    private List<Crocodile> crocodiles = new List<Crocodile>();
+
+    public void CollectNCrocodiles(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"\nCollecting details for crocodile {i + 1}:");
+            Crocodile croc = Crocodile.CollectCrocodileDetails();
+            while (!Crocodile.CheckCrocodileDetails(croc))
+            {
+                Console.WriteLine("Invalid details entered. Please re-enter the details for the crocodile.");
+                croc = Crocodile.CollectCrocodileDetails();
+            }
+            crocodiles.Add(croc);
+        }
+    }
+
+    public void DisplayNCrocodiles()
+    {
+        if (crocodiles == null || crocodiles.Count == 0)
+        {
+            Console.WriteLine("No crocodiles to display.");
+            return;
+        }
+        for (int i = 0; i < crocodiles.Count; i++)
+        {
+            Console.WriteLine($"\nDetails of crocodile {i + 1}:");
+            crocodiles[i].DisplayDetails();
+        }
+    }
+}
